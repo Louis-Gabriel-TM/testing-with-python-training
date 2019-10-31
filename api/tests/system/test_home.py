a846f5988c672de60e90b5c8dd5e5158eab8c196
loic-mangin <loic.mangin@naxos.fr>
2019-10-31 16:44:20 +0100

    Testing: add a Flask.test_client() for the home route

---
+++ api/tests/system/test_home.py
@@ -0,0 +1,17 @@
+import json
+from unittest import TestCase
+
+from app import app
+
+
+class TestHome(TestCase):
+
+    def test_home(self):
+        with app.test_client() as c:
+            response = c.get('/')
+
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(
+                json.loads(response.get_data()),
+                {'message': "Hello world!"}
+            )
