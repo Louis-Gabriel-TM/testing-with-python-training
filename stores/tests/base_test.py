7d8991824a746b263465cbc7d6dc95903512ca19
loic-mangin <loic.mangin@naxos.fr>
2019-11-05 08:45:53 +0100

    Testing: add a BaseTest for future non-unit tests

---
+++ stores/tests/base_test.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+
+from app import app
+from db import db
+
+
+class BaseTest(TestCase):
+    """
+    This class is the parent class to each non-unit test.
+    It allows the instantiation of the database dynamically
+    and makes sure that it is a new blank database each time.
+    """
+
+    def setUp(self):
+        # Make sure the database exists
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+        with app.app_context():
+            db.init_app(app)
+            db.create_all()
+
+        # Get a test client
+        self.app = app.test_client()
+        self.app_context = app.app_context
+
+    def tearDown(self):
+        # Insure the database is blank
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
