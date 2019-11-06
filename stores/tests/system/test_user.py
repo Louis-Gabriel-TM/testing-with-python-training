ac5b5bfc4104fa42637412614023365ba570d49b
Louis Gabriel <louis.gabriel@orange.fr>
2019-11-06 16:17:50 +0100

    Testing: add system tests for User resource

--- stores/tests/system/test_user.py
+++ stores/tests/system/test_user.py
@@ -9,7 +9,7 @@ class TestUser(BaseTest):
     def test_register_user(self):
         with self.app() as client:
             with self.app_context():
-                request = client.post(
+                response = client.post(
                     '/register',
                     data={
                         'username': 'test username',
@@ -17,17 +17,58 @@ class TestUser(BaseTest):
                     }
                 )
 
-                self.assertEqual(request.status_code, 201)
+                self.assertEqual(response.status_code, 201)
                 self.assertIsNotNone(
                     UserModel.find_by_username('test username')
                 )
                 self.assertDictEqual(
-                    json.loads(request.data),
+                    json.loads(response.data),
                     {'message': "User created successfully."}
                 )
 
     def test_register_and_login(self):
-        pass
+        with self.app() as client:
+            with self.app_context():
+                client.post(
+                    '/register',
+                    data={
+                        'username': 'test username',
+                        'password': 'test password'
+                    }
+                )
+                auth_response = client.post(
+                    '/auth',
+                    data=json.dumps({
+                        'username': 'test username',
+                        'password': 'test password'
+                    }),
+                    headers={'Content-Type': 'application/json'}
+                )
+
+                self.assertIn(
+                    'access_token', json.loads(auth_response.data).keys()
+                )
 
     def test_register_duplicate_user(self):
-        pass
+        with self.app() as client:
+            with self.app_context():
+                client.post(
+                    '/register',
+                    data={
+                        'username': 'test username',
+                        'password': 'test password'
+                    }
+                )
+                response = client.post(
+                    '/register',
+                    data={
+                        'username': 'test username',
+                        'password': 'test password'
+                    }
+                )
+
+                self.assertEqual(response.status_code, 400)
+                self.assertDictEqual(
+                    json.loads(response.data),
+                    {'message': "A user with that username already exists."}
+                )
